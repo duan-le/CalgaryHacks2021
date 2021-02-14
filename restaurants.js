117059d13a4d6ab92f9fdea64bf4c6848d60fe4b
duan-le <duanlecontacts@gmail.com>
2021-02-13 22:09:45 -0700

    Semi working restaurant page

---
+++ restaurants.js
@@ -0,0 +1,37 @@
+function loadJSON(callback) {
+  var xobj = new XMLHttpRequest();
+  xobj.overrideMimeType("application/json");
+  xobj.open("GET", "restaurantsData.json", true); // TODO: Change back to correct JSON file
+  xobj.onreadystatechange = function () {
+    if (xobj.readyState == 4 && xobj.status == "200") {
+      callback(JSON.parse(xobj.responseText));
+    }
+  };
+  xobj.send(null);
+}
+
+showRestaurants = (restaurantType) => {
+  loadJSON(function (json) {
+    let restaurants = json.filter((entry) => {
+      return entry.type == restaurantType;
+    });
+    document.getElementById("restaurantDisplay").innerHTML = null;
+    for (let i = 0; i < restaurants.length; i++) {
+      document.getElementById("restaurantDisplay").innerHTML += (
+        `<div class="itemRestaurant"><div class="itemRestaurantLogo"><img src="${restaurants[i].image}" class="iconDetails"></div><div style="margin-left:60px;" class="itemRestaurantName"><h4>${restaurants[i].name}</h4><div class="itemRestaurantName" style="font-size:.6em">${restaurants[i].location}</div></div></div>`
+      );
+    }
+  });
+};
+
+showAllRestaurants = () => {
+  loadJSON(function (json) {
+    let restaurants = json;
+    document.getElementById("restaurantDisplay").innerHTML = '';
+    for (let i = 0; i < restaurants.length; i++) {
+      document.getElementById("restaurantDisplay").innerHTML += (
+        `<div class="itemRestaurant"><div class="itemRestaurantLogo"><img src="${restaurants[i].image}" class="iconDetails"></div><div style="margin-left:60px;" class="itemRestaurantName"><h4>${restaurants[i].name}</h4><div class="itemRestaurantName" style="font-size:.6em">${restaurants[i].location}</div></div></div>`
+      );
+    }
+  });
+};
